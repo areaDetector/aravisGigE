3b980ef54c2205a6525bf06bd80b3c6f45be411b
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2011-07-18 16:13:06 +0000

    aravisGigE: added in 0.1.10 vendor changes

---
+++ aravis/src/arvgcport.c
@@ -0,0 +1,102 @@
+/* Aravis - Digital camera library
+ *
+ * Copyright © 2009-2010 Emmanuel Pacaud
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Emmanuel Pacaud <emmanuel@gnome.org>
+ */
+
+/**
+ * SECTION: arvgcport
+ * @short_description: Class for Port nodes
+ */
+
+#include <arvgcport.h>
+#include <arvdevice.h>
+#include <arvgc.h>
+
+static GObjectClass *parent_class = NULL;
+
+static const char *
+arv_gc_port_get_node_name (ArvGcNode *node)
+{
+	return "Port";
+}
+
+void
+arv_gc_port_read (ArvGcPort *port, void *buffer, guint64 address, guint64 length)
+{
+	ArvGc *genicam;
+	ArvDevice *device;
+
+	g_return_if_fail (ARV_IS_GC_PORT (port));
+
+	genicam = arv_gc_node_get_genicam (ARV_GC_NODE (port));
+	device = arv_gc_get_device (genicam);
+
+	arv_device_read_memory (device, address, length, buffer);
+}
+
+void
+arv_gc_port_write (ArvGcPort *port, void *buffer, guint64 address, guint64 length)
+{
+	ArvGc *genicam;
+	ArvDevice *device;
+
+	g_return_if_fail (ARV_IS_GC_PORT (port));
+
+	genicam = arv_gc_node_get_genicam (ARV_GC_NODE (port));
+	device = arv_gc_get_device (genicam);
+
+	arv_device_write_memory (device, address, length, buffer);
+}
+
+ArvGcNode *
+arv_gc_port_new (void)
+{
+	ArvGcNode *node;
+
+	node = g_object_new (ARV_TYPE_GC_PORT, NULL);
+
+	return node;
+}
+
+static void
+arv_gc_port_init (ArvGcPort *gc_port)
+{
+}
+
+static void
+arv_gc_port_finalize (GObject *object)
+{
+	parent_class->finalize (object);
+}
+
+static void
+arv_gc_port_class_init (ArvGcPortClass *port_class)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (port_class);
+	ArvGcNodeClass *node_class = ARV_GC_NODE_CLASS (port_class);
+
+	parent_class = g_type_class_peek_parent (port_class);
+
+	object_class->finalize = arv_gc_port_finalize;
+
+	node_class->get_node_name = arv_gc_port_get_node_name;
+}
+
+G_DEFINE_TYPE (ArvGcPort, arv_gc_port, ARV_TYPE_GC_NODE)
---
+++ aravis/tests/fake.c
@@ -0,0 +1,126 @@
+#include <glib.h>
+#include <arv.h>
+
+static void
+load_fake_camera_genicam_test (void)
+{
+	const char *genicam_xml;
+	size_t size;
+
+	genicam_xml = arv_get_fake_camera_genicam_xml (&size);
+	g_assert (genicam_xml != NULL);
+	g_assert (size != 0);
+
+	genicam_xml = arv_get_fake_camera_genicam_xml (NULL);
+	g_assert (genicam_xml != NULL);
+}
+
+static void
+trigger_registers_test (void)
+{
+	ArvDevice *device;
+	ArvGc *genicam;
+	ArvGcNode *node;
+	gint64 address;
+
+	device = arv_fake_device_new ("TEST0");
+	g_assert (ARV_IS_FAKE_DEVICE (device));
+
+	genicam = arv_device_get_genicam (device);
+	g_assert (ARV_IS_GC (genicam));
+
+	node = arv_gc_get_node (genicam, "TriggerModeRegister");
+	g_assert (ARV_IS_GC_NODE (node));
+
+	address = arv_gc_register_get_address (ARV_GC_REGISTER (node));
+	g_assert_cmpint (address, ==, ARV_FAKE_CAMERA_REGISTER_TRIGGER_MODE);
+
+	address = arv_gc_register_get_address (ARV_GC_REGISTER (arv_gc_get_node (genicam,
+										 "TriggerSourceRegister")));
+	g_assert_cmpint (address, ==, ARV_FAKE_CAMERA_REGISTER_TRIGGER_SOURCE);
+
+	address = arv_gc_register_get_address (ARV_GC_REGISTER (arv_gc_get_node (genicam,
+										 "TriggerActivationRegister")));
+	g_assert_cmpint (address, ==, ARV_FAKE_CAMERA_REGISTER_TRIGGER_ACTIVATION);
+
+	arv_device_set_string_feature_value (device, "TriggerSelector", "AcquisitionStart");
+
+	address = arv_gc_register_get_address (ARV_GC_REGISTER (node));
+	g_assert_cmpint (address, ==, ARV_FAKE_CAMERA_REGISTER_TRIGGER_MODE +
+			 ARV_FAKE_CAMERA_REGISTER_ACQUISITION_START_OFFSET);
+
+	g_object_unref (device);
+}
+
+static void
+fake_device_test (void)
+{
+	ArvDevice *device;
+	int int_value;
+	double dbl_value;
+
+	device = arv_fake_device_new ("TEST0");
+	g_assert (ARV_IS_FAKE_DEVICE (device));
+
+	/* Check default */
+	int_value = arv_device_get_integer_feature_value (device, "Width");
+	g_assert_cmpint (int_value, ==, ARV_FAKE_CAMERA_WIDTH_DEFAULT);
+
+	arv_device_set_integer_feature_value (device, "Width", 1024);
+	int_value = arv_device_get_integer_feature_value (device, "Width");
+	g_assert_cmpint (int_value, ==, 1024);
+
+	/* Check default */
+	int_value = arv_device_get_integer_feature_value (device, "Height");
+	g_assert_cmpint (int_value, ==, ARV_FAKE_CAMERA_HEIGHT_DEFAULT);
+
+	arv_device_set_integer_feature_value (device, "Height", 1024);
+	int_value = arv_device_get_integer_feature_value (device, "Height");
+	g_assert_cmpint (int_value, ==, 1024);
+
+	int_value = arv_device_get_integer_feature_value (device, "BinningHorizontal");
+	g_assert_cmpint (int_value, ==, ARV_FAKE_CAMERA_BINNING_HORIZONTAL_DEFAULT);
+	int_value = arv_device_get_integer_feature_value (device, "BinningVertical");
+	g_assert_cmpint (int_value, ==, ARV_FAKE_CAMERA_BINNING_VERTICAL_DEFAULT);
+	int_value = arv_device_get_integer_feature_value (device, "PixelFormat");
+	g_assert_cmpint (int_value, ==, ARV_FAKE_CAMERA_PIXEL_FORMAT_DEFAULT);
+
+	dbl_value = arv_device_get_float_feature_value (device, "AcquisitionFrameRate");
+	g_assert_cmpfloat (dbl_value, ==, ARV_FAKE_CAMERA_ACQUISITION_FRAME_RATE_DEFAULT);
+	dbl_value = arv_device_get_float_feature_value (device,  "ExposureTimeAbs");
+	g_assert_cmpfloat (dbl_value, ==, ARV_FAKE_CAMERA_EXPOSURE_TIME_US_DEFAULT);
+
+	int_value = arv_device_get_integer_feature_value (device, "GainRaw");
+	g_assert_cmpint (int_value, ==, 0);
+	int_value = arv_device_get_integer_feature_value (device, "GainAuto");
+	g_assert_cmpint (int_value, ==, 1);
+
+	int_value = arv_device_get_integer_feature_value (device, "PayloadSize");
+	g_assert_cmpint (int_value, ==, 1024 * 1024);
+
+	g_object_unref (device);
+}
+
+int
+main (int argc, char *argv[])
+{
+	int result;
+	int i;
+
+	g_test_init (&argc, &argv, NULL);
+
+	g_type_init ();
+
+	arv_set_fake_camera_genicam_filename (GENICAM_FILENAME);
+
+	g_test_add_func ("/fake/load-fake-camera-genicam", load_fake_camera_genicam_test);
+	g_test_add_func ("/fake/trigger-registers", trigger_registers_test);
+	g_test_add_func ("/fake/fake-device", fake_device_test);
+
+	result = g_test_run();
+
+	arv_shutdown ();
+
+	return result;
+}
+
