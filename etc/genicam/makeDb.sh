f59aa8fc7698045db96c59216647679205fd862c
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2011-07-19 14:18:14 +0000

    aravisGigE: added Flea3 camera Db and screen

---
+++ etc/genicam/makeDb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../../aravisGigEApp/src/makeDbAndEdl.py Prosilica_GC1020C.xml Prosilica_GC
+../../aravisGigEApp/src/makeDbAndEdl.py AVT_Manta_G125B.xml AVT_Manta
+../../aravisGigEApp/src/makeDbAndEdl.py Baumer_TXG08c.xml Baumer_TXG
+../../aravisGigEApp/src/makeDbAndEdl.py JAI_RM-6740GE.xml JAI_6740
+../../aravisGigEApp/src/makeDbAndEdl.py PGR_Flea3.xml PGR_Flea3
