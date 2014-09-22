16dc372710d20192b735388c74e3ab9d33c542c9
Mark Rivers <rivers@cars.uchicago.edu>
2014-09-22 12:55:18 -0500

    Fixes

--- iocs/aravisGigEIOC/iocBoot/iocAravisGigE/st.cmd
+++ iocs/aravisGigEIOC/iocBoot/iocAravisGigE/st.cmd
@@ -4,14 +4,15 @@ errlogInit(20000)
 dbLoadDatabase("$(TOP)/dbd/aravisGigEApp.dbd")
 aravisGigEApp_registerRecordDeviceDriver(pdbbase) 
 
-epicsEnvSet("PREFIX", "13ARAV1:")
+epicsEnvSet("PREFIX", "13ARV1:")
 epicsEnvSet("PORT",   "ARV1")
 epicsEnvSet("QSIZE",  "20")
 epicsEnvSet("XSIZE",  "1920")
 epicsEnvSet("YSIZE",  "2048")
 epicsEnvSet("NCHANS", "2048")
 
-aravisCameraConfig("$(PORT)", "Prosilica-02-2131A-06202")
+#aravisCameraConfig("$(PORT)", "Prosilica-02-2131A-06202")
+aravisCameraConfig("$(PORT)", "Point Grey Research-14273040")
 dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ADCORE)/db/NDFile.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ARAVISGIGE)/db/Prosilica_GC.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,CAMSERVER_PORT=camserver")
@@ -19,7 +20,8 @@ dbLoadRecords("$(ARAVISGIGE)/db/Prosilica_GC.template","P=$(PREFIX),R=cam1:,PORT
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
 dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int32,FTVL=LONG,NELEMENTS=94965")
+# Allow for cameras up to 2048x2048x3 for RGB
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=1258912")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
